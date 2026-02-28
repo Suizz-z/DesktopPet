9bfc5bdab6d6b21499240f9456d385dd357e60cd
Suiz <16091019+suiz-flow@user.noreply.gitee.com>
2026-03-01 01:44:48 +0800

    feat(CMAgent): 新增创建桌宠人设细节功能
    添加create_details_system_prompt.md系统提示词文件
    在CMAgent类中增加创建桌宠人设细节的方法createPet
    支持从桌宠故事和世界设定生成人设细节

--- app/utils/CM/cm_agent.py
+++ app/utils/CM/cm_agent.py
@@ -9,10 +9,27 @@ from app.utils.DB.db import PetAIDB
 
 from langchain.agents import create_agent
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
+from langchain.agents.structured_output import ToolStrategy
+
+pet_details = {
+    "type":"object",
+    "properties":{
+        "pet_personality":{
+            "type":"string",
+            "description":"桌宠的性格"
+        },
+        "pet_speech_style":{
+            "type":"string",
+            "description":"桌宠的沟通风格"
+        }
+    }
+}
 
 class CMAgent:
     def __init__(self, pet_name, world_prompt):
         self.pet_name = pet_name
+        self.pet_story = ""
+        self.pet_details = ""
         self.world_prompt = world_prompt
         self.world = ""
         self.db = PetAIDB()
@@ -23,7 +40,7 @@ class CMAgent:
         self.world_user_prompt = PromptTemplate.from_file(template_file=os.path.join(prompts_dir, "create_world_user_prompt copy.md"),encoding="utf-8").template
         self.role_system_prompt = PromptTemplate.from_file(template_file=os.path.join(prompts_dir, "create_role_system_prompt.md"),encoding="utf-8").template
         self.role_user_prompt = PromptTemplate.from_file(template_file=os.path.join(prompts_dir, "create_role_user_prompt.md"),encoding="utf-8").template
-        
+        self.details_system_prompt = PromptTemplate.from_file(template_file=os.path.join(prompts_dir, "create_details_system_prompt.md"),encoding="utf-8").template
 
     def createWorldAgent(self):
         user_prompt = self.world_user_prompt.format(world_prompt=self.world_prompt)
@@ -44,14 +61,30 @@ class CMAgent:
         )
         for chunk in doubao_chat_agent.stream({"messages": [("user", user_prompt)]}, stream_mode="messages"):
             if chunk[0].content:
+                self.pet_story += chunk[0].content  
                 yield chunk[0].content
-        
+    
+    def createPet(self):
+        tools = []
+        agent = create_agent(
+            model=self.doubaoLLM,
+            tools=tools,
+            response_format=ToolStrategy(pet_details),
+            system_prompt=self.details_system_prompt,
+        )
+        self.pet_details = agent.invoke({"messages": [("user", f"桌宠故事：{self.pet_story}，世界设定：{self.world}")]})
+
+    
 
 if __name__ == "__main__":
     cm_agent = CMAgent("小明", "武侠世界")
     world_agent = cm_agent.createWorldAgent()
     for chunk in world_agent:
         print(chunk, end="", flush=True)
+    print("\n")
     role_agent = cm_agent.createRoleAgent()
     for chunk in role_agent:
         print(chunk, end="", flush=True)
+    print("\n")
+    cm_agent.createPet()
+    print(cm_agent.pet_details['structured_response'])
